10ecc8f0e55f98b1a3c97d0d21919cf788214ce1
jimhome1608@gmail.com <jimhome1608@gmail.com>
2015-12-22 18:03:56 +1100

    ClientData and Vibration all going well

--- eMailChecker1.pas
+++ eMailChecker1.pas
@@ -9,12 +9,18 @@ uses
   IdExplicitTLSClientServerBase, IdMessageClient, IdPOP3,
   FMX.Controls.Presentation, FMX.StdCtrls, IdIOHandler, IdIOHandlerSocket,
   IdIOHandlerStack, IdSSL, IdSSLOpenSSL, IdIMAP4, FMX.Edit, FMX.ScrollBox,
-  FMX.Memo, IdMessage, FMX.Layouts;
+  FMX.Memo, IdMessage, FMX.Layouts, IdText, FMX.TabControl, Data.DB,
+  Datasnap.DBClient, System.IOUtils
+  {$IFDEF ANDROID}
+    ,Androidapi.JNI.Os,
+    Androidapi.JNI.GraphicsContentViewText,
+    Androidapi.Helpers,
+    Androidapi.JNIBridge
+  {$ENDIF};
 
 type
   TfrmEmailChecker = class(TForm)
     IdPOP31: TIdPOP3;
-    Memo1: TMemo;
     IdMessage1: TIdMessage;
     ToolBar1: TToolBar;
     Layout1: TLayout;
@@ -22,9 +28,18 @@ type
     Layout2: TLayout;
     lblMessageCount: TLabel;
     lblReadingMessageNumber: TLabel;
+    tlcDiana: TTabControl;
+    tiSummary: TTabItem;
+    TabItem2: TTabItem;
+    Memo1: TMemo;
+    tiData: TTabItem;
+    Edit1: TEdit;
+    ClientDataSet1: TClientDataSet;
+    Button2: TButton;
     procedure Button1Click(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
   private
     sl: TstringList;
   public
@@ -42,6 +57,9 @@ procedure TfrmEmailChecker.Button1Click(Sender: TObject);
 var _CheckMessages: integer;
     _MessageNumber: integer;
     _header: string;
+    _partsIdx: integer;
+    _partType:string;
+    _rawMessage: string;
      //admin@ereal.com.au
 begin
    IdPOP31.Connect;
@@ -53,8 +71,13 @@ begin
      _header:= IdMessage1.Headers.Text;
      _header:= lowercase(_header);
      if pos('admin@ereal.com.au',_header) > 0 then begin
-       memo1.Lines.Add(_header);
-       Break;
+       //memo1.Lines.Add(_header);
+      // IdPOP31.Retrieve(_MessageNumber,IdMessage1);
+       IdPOP31.Top(_MessageNumber,sl,100);
+//       IdPOP31.RetrieveRaw(_MessageNumber,sl);
+       _rawMessage:= sl.Text;
+       memo1.Lines.Add(_rawMessage);
+       break;
      end;
    end;
   // IdPOP31.RetrieveRaw(_CheckMessages,sl);
@@ -62,9 +85,37 @@ begin
 
 end;
 
-procedure TfrmEmailChecker.FormCreate(Sender: TObject);
+
+
+
+
+procedure TfrmEmailChecker.Button2Click(Sender: TObject);
+{$IFDEF ANDROID}
+Var Vibrator: JVibrator;
+{$ENDIF}
 begin
-   sl:= TstringList.Create;
+    {$IFDEF ANDROID}
+    Vibrator:=TJVibrator.Wrap((SharedActivityContext.getSystemService(TJContext.JavaClass.VIBRATOR_SERVICE) as ILocalObject).GetObjectID);
+    Vibrator.vibrate(500);
+    {$ENDIF}
+end;
+
+procedure TfrmEmailChecker.FormCreate(Sender: TObject);
+var data_file: string;
+begin
+  data_file := TPath.Combine(TPath.GetDocumentsPath, 'data_email_checker');
+  if fileExists(data_file) then
+    ClientDataSet1.LoadFromFile(data_file)
+   else begin
+      ClientDataSet1.FieldDefs.Add('FirstName', ftString, 20);
+      ClientDataSet1.FieldDefs.Add('LastName', ftString, 20);
+      ClientDataSet1.CreateDataSet;
+   end;
+   ClientDataSet1.Active := True;
+   ClientDataSet1.AppendRecord(['John', 'Smith']);
+   ClientDataSet1.AppendRecord(['Jane', 'Doe']);
+   ClientDataSet1.SaveToFile(data_file);
+   Edit1.Text:= intToSTr( ClientDataSet1.RecordCount);   sl:= TstringList.Create;
 end;
 
 procedure TfrmEmailChecker.FormDestroy(Sender: TObject);
