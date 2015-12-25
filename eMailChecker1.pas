aaa9f5a0642df3680375b79598982b08b54bd1f4
jimhome1608@gmail.com <jimhome1608@gmail.com>
2015-12-25 18:20:29 +1100

    Looking much better and working well. Almost ready for release
    Looking much better and working well. Almost ready for release.
    Works OK on desktop too although does not adjust to high res screens so
    well.

--- eMailChecker1.pas
+++ eMailChecker1.pas
@@ -33,9 +33,13 @@ type
     imgDiana: TImage;
     imgCross: TImage;
     imgREA: TImage;
-    imgRVW: TImage;
     imgDOMAIN: TImage;
+    Rectangle1: TRectangle;
     Label1: TLabel;
+    Layout2: TLayout;
+    Rectangle3: TRectangle;
+    lblLastCheck: TLabel;
+    imgArrow: TImage;
     procedure Button1Click(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
@@ -48,6 +52,7 @@ type
     function write_client_dataset_lastfound(_filter: string; _lastfound: TdateTime; _MessageNumber: integer): string;
     function read_client_dataset(_filter, _FieldName: string): string;
     function read_hours_since_lastfound(_filter: string): double;
+    function read_time_lastfound(_filter: string): string;
     function filter_dateset_on(_filter: string): integer;
   public
     { Public declarations }
@@ -106,7 +111,6 @@ begin
    ClientDataSet1.Post;
    ClientDataSet1.SaveToFile(data_file);
    refresListView;
-
 end;
 
 
@@ -140,6 +144,20 @@ begin
      Result:= (now-_LAST_FOUND) *24;
 end;
 
+function TfrmEmailChecker.read_time_lastfound(_filter: string): string;
+var _LAST_FOUND: TDateTime;
+    _Hours: double;
+     H, M, S, MS: Word;
+begin
+     filter_dateset_on(_filter);
+     _LAST_FOUND:=  ClientDataSet1.FieldByName('LAST_FOUND').AsDateTime;
+     _Hours:= (now-_LAST_FOUND) *24;
+     if _Hours  < 12 then
+        result:= formatDateTime('hh:nnam/pm',_LAST_FOUND)
+     else
+        result:= formatDateTime('dd/mm hh:nnam/pm',_LAST_FOUND);
+end;
+
 procedure TfrmEmailChecker.refresListView;
 var _LItem: TListViewItem;
     _ListItemImage: TListItemImage;
@@ -150,28 +168,28 @@ begin
     DecodeTime(Now, H, M, S, MS);
 
     _HoursAgo_VERSION:= read_hours_since_lastfound('VERSION');
+    _HoursAgo_REA:= read_hours_since_lastfound('REA');
+    _HoursAgo_DOMAIN:= read_hours_since_lastfound('DOMAIN');
+    _HoursAgo_RVW:= read_hours_since_lastfound('RVW');
+
+    lblLastCheck.Text:=   format('Last check: %f hour ago',[_HoursAgo_VERSION]);
 
     lbxData.Items.Clear;
-    _LItem := lbxData.Items.Add;
-   _LItem.Text := format('      Last check: %f hour ago',[_HoursAgo_VERSION]);
-
 
    _LItem := lbxData.Items.Add;
-   _LItem.Text := 'REA: '+read_client_dataset('REA','LAST_FOUND');
-   _LItem.BitmapRef := imgREA.Bitmap;
+   _LItem.Objects.AccessoryObject.Visible := False;
+   _LItem.Text := 'REALESTATE  '+read_time_lastfound('REA');
+   _LItem.BitmapRef := imgArrow.Bitmap;
    _ListItemImage:=  (_LItem.Objects.FindDrawable(sThumbNailName) as TListItemImage);
    if _ListItemImage <> Nil then begin
       _ListItemImage.OwnsBitmap := False;
-      _ListItemImage.Bitmap := imgREA.Bitmap;
+      _ListItemImage.Bitmap := imgArrow.Bitmap;
    end;
-
-   _HoursAgo_REA:= read_hours_since_lastfound('REA');
-   _HoursAgo_DOMAIN:= read_hours_since_lastfound('DOMAIN');
-   _HoursAgo_RVW:= read_hours_since_lastfound('RVW');
 
 
    _LItem := lbxData.Items.Add;
-   _LItem.Text :=format('%f hour ago',[_HoursAgo_REA]);
+   _LItem.Objects.AccessoryObject.Visible := False;
+   _LItem.Text :=format('  %f hour ago',[_HoursAgo_REA]);
    _Image:=  imgGreenTick;
    if (_HoursAgo_REA - _HoursAgo_RVW) > 1   then begin
      _Image:=  imgCross; //imgGreenTick;
@@ -179,7 +197,7 @@ begin
    if (_HoursAgo_REA > 24)   then begin
      _Image:=  imgCross; //imgGreenTick;
    end;
-   if (_HoursAgo_REA > 4) and (H > 11) and ( H < 18)   then begin
+   if (_HoursAgo_REA > 4) and (H > 11) and ( H < 20)   then begin
      _Image:=  imgCross; //imgGreenTick;
    end;
 
@@ -192,22 +210,25 @@ begin
    end;
 
    _LItem := lbxData.Items.Add;
-   _LItem.Text := 'DOMAIN: '+read_client_dataset('DOMAIN','LAST_FOUND');
-   _LItem.BitmapRef := imgDOMAIN.Bitmap;
+   _LItem.Objects.AccessoryObject.Visible := False;
+   _LItem.Text := 'DOMAIN  '+read_time_lastfound('DOMAIN');
+   _LItem.BitmapRef := imgArrow.Bitmap;
    _ListItemImage:=  (_LItem.Objects.FindDrawable(sThumbNailName) as TListItemImage);
    if _ListItemImage <> Nil then begin
       _ListItemImage.OwnsBitmap := False;
-      _ListItemImage.Bitmap := imgDOMAIN.Bitmap;
+      _ListItemImage.Bitmap := imgArrow.Bitmap;
    end;
+
    _Image:=  imgGreenTick;
    if (_HoursAgo_DOMAIN > 24)   then begin
      _Image:=  imgCross; //imgGreenTick;
    end;
-   if (_HoursAgo_DOMAIN > 4) and (H > 11) and ( H < 18)   then begin
+   if (_HoursAgo_DOMAIN > 4) and (H > 11) and ( H < 20)   then begin
      _Image:=  imgCross; //imgGreenTick;
    end;
    _LItem := lbxData.Items.Add;
-   _LItem.Text := format('%f hour ago',[_HoursAgo_DOMAIN]);
+   _LItem.Objects.AccessoryObject.Visible := False;
+   _LItem.Text := format('  %f hour ago',[_HoursAgo_DOMAIN]);
    _LItem.BitmapRef := _Image.Bitmap;
    _ListItemImage:=  (_LItem.Objects.FindDrawable(sThumbNailName) as TListItemImage);
    if _ListItemImage <> Nil then begin
@@ -216,34 +237,39 @@ begin
    end;
 
    _LItem := lbxData.Items.Add;
-   _LItem.Text := 'RVW: '+read_client_dataset('RVW','LAST_FOUND');
-   _LItem.BitmapRef := imgRVW.Bitmap;
+   _LItem.Objects.AccessoryObject.Visible := False;
+   _LItem.Text := 'REALESTATEVIEW '+read_time_lastfound('RVW');
+   _LItem.BitmapRef := imgArrow.Bitmap;
    _ListItemImage:=  (_LItem.Objects.FindDrawable(sThumbNailName) as TListItemImage);
    if _ListItemImage <> Nil then begin
       _ListItemImage.OwnsBitmap := False;
-      _ListItemImage.Bitmap := imgRVW.Bitmap;
+      _ListItemImage.Bitmap := imgArrow.Bitmap;
    end;
+
    _Image:=  imgGreenTick;
    if (_HoursAgo_RVW > 24)   then begin
      _Image:=  imgCross; //imgGreenTick;
    end;
-   if (_HoursAgo_RVW > 4) and (H > 11) and ( H < 18)   then begin
+   if (_HoursAgo_RVW > 4) and (H > 11) and ( H < 20)   then begin
      _Image:=  imgCross; //imgGreenTick;
    end;
    _LItem := lbxData.Items.Add;
-   _LItem.Text :=format('%f hour ago',[_HoursAgo_RVW]);
+   _LItem.Objects.AccessoryObject.Visible := False;
+   _LItem.Text :=format('  %f hour ago',[_HoursAgo_RVW]);
    _LItem.BitmapRef := _Image.Bitmap;
    _ListItemImage:=  (_LItem.Objects.FindDrawable(sThumbNailName) as TListItemImage);
    if _ListItemImage <> Nil then begin
       _ListItemImage.OwnsBitmap := False;
       _ListItemImage.Bitmap := _Image.Bitmap;
    end;
-
+   {$IFDEF ANDROID}
    _LItem := lbxData.Items.Add;
+   _LItem.Objects.AccessoryObject.Visible := False;
    _LItem.Text :='______________________';
 
    _LItem := lbxData.Items.Add;
-   _LItem.Text := 'DIANA: '+read_client_dataset('DIANA','LAST_FOUND');
+   _LItem.Objects.AccessoryObject.Visible := False;
+   _LItem.Text := 'DIANA: '+read_time_lastfound('DIANA');
    _LItem.BitmapRef := imgDiana.Bitmap;
    _ListItemImage:=  (_LItem.Objects.FindDrawable(sThumbNailName) as TListItemImage);
    if _ListItemImage <> Nil then begin
@@ -252,7 +278,9 @@ begin
    end;
 
    _LItem := lbxData.Items.Add;
+   _LItem.Objects.AccessoryObject.Visible := False;
    _LItem.Text :=format('%f hour ago',[read_hours_since_lastfound('DIANA')]);
+   {$ENDIF}
 
 
 end;
@@ -278,8 +306,12 @@ end;
 procedure TfrmEmailChecker.FormCreate(Sender: TObject);
 
 begin
+  {$IFDEF ANDROID}
+  lbxData.Scale.X:= 1;
+  lbxData.Scale.Y:= 1;
+  {$ENDIF}
   data_file := System.IOUtils.TPath.Combine(System.IOUtils.TPath.GetDocumentsPath, 'data_email_checker');
- //DELETEfILE(data_file);
+//  DELETEfILE(data_file);
   if NOT fileExists(data_file) then BEGIN;
       ClientDataSet1.FieldDefs.Add('WHO', ftString, 255);
       ClientDataSet1.FieldDefs.Add('WHEN', ftString, 255);
